840398c03dd13fdb0f45831b2f0d04b9945996bf
jiraphon <jiraphon@sirimedia.com>
2018-01-04 22:48:38 +0700

    setState  change onclick state

--- src/App.js
+++ src/App.js
@@ -8,13 +8,26 @@ class App extends Component {
       {name: 'Katze', age: 'gato'},
       {name: 'siba', age: 'blue'},
       {name: 'pug', age: 'pink'}
-    ]
+    ],
+    otherState: 'some one'
   }
+
+  switchNameHandler = () => {
+    console.log("click");
+    this.setState({
+      person: [
+        {name: 'Max', age: 'blue'},
+        {name: 'Maximun', age: 'yellow'},
+        {name: 'Mininum', age: 'red'}
+      ]
+    })
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Hi, I'm</h1>
-        <button>Switch Name</button>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
         <Person name={this.state.person[0].name} age={this.state.person[0].age}/>
         <Person name={this.state.person[1].name} age={this.state.person[1].age}>My Hobbies: Music</Person>
         <Person name={this.state.person[2].name} age={this.state.person[2].age}/>
