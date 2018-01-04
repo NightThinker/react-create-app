78b1e90f28003dcc3b8665fd67a99c70cdead721
jiraphon <jiraphon@sirimedia.com>
2018-01-04 23:03:25 +0700

    method function

--- src/App.js
+++ src/App.js
@@ -12,11 +12,11 @@ class App extends Component {
     otherState: 'some one'
   }
 
-  switchNameHandler = () => {
+  switchNameHandler = (newName) => {
     console.log("click");
     this.setState({
       person: [
-        {name: 'Max', age: 'blue'},
+        {name: newName, age: 'blue'},
         {name: 'Maximun', age: 'yellow'},
         {name: 'Mininum', age: 'red'}
       ]
@@ -27,14 +27,14 @@ class App extends Component {
     return (
       <div className="App">
         <h1>Hi, I'm</h1>
-        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <button onClick={() => this.switchNameHandler('anyone super')}>Switch Name</button>
         <Person 
           name={this.state.person[0].name} 
           age={this.state.person[0].age}/>
         <Person 
           name={this.state.person[1].name} 
           age={this.state.person[1].age}
-          click={this.switchNameHandler}>My Hobbies: Music</Person>
+          click={this.switchNameHandler.bind(this, 'howold')}>My Hobbies: Music</Person>
         <Person 
           name={this.state.person[2].name} 
           age={this.state.person[2].age}/>
